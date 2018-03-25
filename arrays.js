dfc2cd8a5d5ccd94cc2315a02373721cd43cb83f
Raymond W. Robinson <raymondrobinson123@gmail.com>
2018-03-25 19:29:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,51 +1,29 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+var chocolateBars=['snickers', 'hungred grand', 'kitkat', 'skittles'];
 
-var numArray = [1,2,3]
-
-function addElementToBeginningOfArray(chocolateBars, temp){
-  const ncb = ['foo', ...chocolateBars]
-  return ncb
+function addElementToBeginningOfArray(array, element){
+  this.element = element;
+  var newArray = [element, ...array];
+  return newArray;
 }
 
-function destructivelyAddElementToBeginningOfArray(chocolateBars, temp){
-  chocolateBars.unshift(temp)
-  return chocolateBars
+function destructivelyAddElementToBeginningOfArray(array, element){
+  this.element = element;
+  array.unshift(element);
+  return array;
 }
 
-function addElementToEndOfArray(chocolateBars, temp){
-  const ncb = [...chocolateBars, 'foo']
-  return ncb
+function addElementToEndOfArray(array, element){
+  var newArray = [...array, element];
+  return newArray;
 }
 
-function destructivelyAddElementToEndOfArray(chocolateBars, temp){
-  chocolateBars.push(temp)
-  return chocolateBars
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
 }
- function accessElementInArray(){
-   var newArray = [1,2,3]
-   return newArray[2]
- }
- 
- function destructivelyRemoveElementFromBeginningOfArray(array){
-   array.shift()
-   return array
- }
- 
- function removeElementFromBeginningOfArray(){
-   var newArray = [1,2,3]
-   const mNewArray = newArray.slice(1)
-   return mNewArray
- }
- 
- function destructivelyRemoveElementFromEndOfArray(array){
-   array.pop()
-   return array
- }
- 
- function removeElementFromEndOfArray(){
-   var newArray = [1,2,3]
-   const mNewArray = newArray.slice(0, newArray.length - 1)
-   return mNewArray
- }
- 
- 
+
+function accessElementInArray(array,index){
+  this.array = [1,2,index]
+  this.index = 3
+  console.log(array[2]);
+}
