fb2647078c75ef52e6ce60a140036781194e668c
Raymond W. Robinson <raymondrobinson123@gmail.com>
2018-03-25 19:41:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,7 +29,7 @@ function accessElementInArray(array, index){
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  array = chocolateBars;
+  array = [1,2,3];
   array.shift();
   return array;
 }
